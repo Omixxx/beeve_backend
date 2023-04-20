4bff6cf20db77ebff547eb8fbc40d3a04aa231f4
Omixxx <xxcortex99@gmail.com>
2023-04-20 22:40:13 +0200

    - Add logs in ApiExceptionHandler class

--- vino/src/main/java/it/unimol/vino/exceptions/ApiExceptionHandler.java
+++ vino/src/main/java/it/unimol/vino/exceptions/ApiExceptionHandler.java
@@ -1,5 +1,8 @@
 package it.unimol.vino.exceptions;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -12,106 +15,119 @@ import java.time.ZonedDateTime;
 @ControllerAdvice
 public class ApiExceptionHandler {
 
+    protected final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+
     @ExceptionHandler(value = {ApiRequestException.class})
-    public ResponseEntity<Object> handleApiRequestException(ApiRequestException e) {
+    protected ResponseEntity<Object> handleApiRequestException(ApiRequestException e) {
         ApiException apiException = new ApiException(
                 e.getMessage(),
                 HttpStatus.BAD_REQUEST,
                 ZonedDateTime.now()
         );
+        LOGGER.error(ExceptionUtils.getStackTrace(e));
         return new ResponseEntity<>(apiException, HttpStatus.BAD_REQUEST);
     }
 
 
     @ExceptionHandler(value = {UsernameNotFoundException.class})
-    public ResponseEntity<Object> handleApiRequestException(UsernameNotFoundException e) {
+    protected ResponseEntity<Object> handleApiRequestException(UsernameNotFoundException e) {
         ApiException apiException = new ApiException(
                 e.getMessage(),
                 HttpStatus.NOT_FOUND,
                 ZonedDateTime.now()
         );
+        LOGGER.error(ExceptionUtils.getStackTrace(e));
         return new ResponseEntity<>(apiException, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(value = {UserNotFoundException.class})
-    public ResponseEntity<Object> handleApiRequestException(UserNotFoundException e) {
+    protected ResponseEntity<Object> handleApiRequestException(UserNotFoundException e) {
         ApiException apiException = new ApiException(
                 e.getMessage(),
                 HttpStatus.NOT_FOUND,
                 ZonedDateTime.now()
         );
+        LOGGER.error(ExceptionUtils.getStackTrace(e));
         return new ResponseEntity<>(apiException, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(value = {UserAlreadyRegistered.class})
-    public ResponseEntity<Object> handleApiRequestException(UserAlreadyRegistered e) {
+    protected ResponseEntity<Object> handleApiRequestException(UserAlreadyRegistered e) {
         ApiException apiException = new ApiException(
                 e.getMessage(),
                 HttpStatus.CONFLICT,
                 ZonedDateTime.now()
         );
+        LOGGER.error(ExceptionUtils.getStackTrace(e));
         return new ResponseEntity<>(apiException, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(value = {PasswordNotValidException.class})
-    public ResponseEntity<Object> handleApiRequestException(PasswordNotValidException e) {
+    protected ResponseEntity<Object> handleApiRequestException(PasswordNotValidException e) {
         ApiException apiException = new ApiException(
                 e.getMessage(),
                 HttpStatus.BAD_REQUEST,
                 ZonedDateTime.now()
         );
+        LOGGER.error(ExceptionUtils.getStackTrace(e));
         return new ResponseEntity<>(apiException, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = {PermissionNotFound.class})
-    public ResponseEntity<Object> handleApiRequestException(PermissionNotFound e) {
+    protected ResponseEntity<Object> handleApiRequestException(PermissionNotFound e) {
         ApiException apiException = new ApiException(
                 e.getMessage(),
                 HttpStatus.NOT_FOUND,
                 ZonedDateTime.now()
         );
+        LOGGER.error(ExceptionUtils.getStackTrace(e));
         return new ResponseEntity<>(apiException, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(value = {PermissionAlreadyAssigned.class})
-    public ResponseEntity<Object> handleApiRequestException(PermissionAlreadyAssigned e) {
+    protected ResponseEntity<Object> handleApiRequestException(PermissionAlreadyAssigned e) {
         ApiException apiException = new ApiException(
                 e.getMessage(),
                 HttpStatus.CONFLICT,
                 ZonedDateTime.now()
         );
+        LOGGER.error(ExceptionUtils.getStackTrace(e));
         return new ResponseEntity<>(apiException, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(value = {PermissionAlreadyExist.class})
-    public ResponseEntity<Object> handleApiRequestException(PermissionAlreadyExist e) {
+    protected ResponseEntity<Object> handleApiRequestException(PermissionAlreadyExist e) {
         ApiException apiException = new ApiException(
                 e.getMessage(),
                 HttpStatus.CONFLICT,
                 ZonedDateTime.now()
         );
+        LOGGER.error(ExceptionUtils.getStackTrace(e));
         return new ResponseEntity<>(apiException, HttpStatus.CONFLICT);
     }
 
 
     @ExceptionHandler(value = {SectorNotFoundException.class})
-    public ResponseEntity<Object> handleApiRequestException(SectorNotFoundException e) {
+    protected ResponseEntity<Object> handleApiRequestException(SectorNotFoundException e) {
         ApiException apiException = new ApiException(
                 e.getMessage(),
                 HttpStatus.NOT_FOUND,
                 ZonedDateTime.now()
         );
+        LOGGER.error(ExceptionUtils.getStackTrace(e));
         return new ResponseEntity<>(apiException, HttpStatus.NOT_FOUND);
     }
 
 
     @ExceptionHandler(value = {HttpMessageNotReadableException.class})
-    public ResponseEntity<Object> handleApiRequestException(HttpMessageNotReadableException e) {
+    protected ResponseEntity<Object> handleApiRequestException(HttpMessageNotReadableException e) {
         ApiException apiException = new ApiException(
                 e.getMessage(),
                 HttpStatus.BAD_REQUEST,
                 ZonedDateTime.now()
         );
+        LOGGER.error(ExceptionUtils.getStackTrace(e));
         return new ResponseEntity<>(apiException, HttpStatus.BAD_REQUEST);
     }
 }
