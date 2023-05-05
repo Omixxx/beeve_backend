434b6205359f52ba0790f4bc2eed67bf5a2d7ed8
Angeloconca <angelo.conca99@gmail.com>
2023-05-05 15:25:11 +0200

    aggiunte nuove classi di test e aggiunte nuove dipendenze al pom,grapeType e contribution mi fanno creare attraverso i costruttori degli ogetti non validi,da verificare se il comportamento è normale oppure no

--- vino/src/test/java/it/unimol/vino/entity/ContributionEntityTest.java
+++ vino/src/test/java/it/unimol/vino/entity/ContributionEntityTest.java
@@ -1,31 +1,109 @@
 package it.unimol.vino.entity;
+
 import it.unimol.vino.models.entity.Contribution;
 import it.unimol.vino.models.entity.GrapeType;
-import org.junit.jupiter.api.*;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+//mi fa creare classi con valori non validi
 public class ContributionEntityTest {
 
+    private Validator validator;
+
+    @BeforeEach
+    void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
     @Test
-    void testConstructorAndGetters() {
-        Long id = 1L;
-        String origin = "Italia";
-        String country = "Italia";
-        String photoURL = "http://example.com/photo.jpg";
-        String description = "Questo è un contributo";
-        double sugarDegree = 12.3;
-        double quantity = 45.6;
-        GrapeType grapeType = new GrapeType();
+    public void testContributionWithNegativeQuantityIsInvalid() {
+        Contribution contribution = Contribution.builder()
+                .origin("Italy")
+                .country("Italy")
+                .photoURL("http://example.com/image.jpg")
+                .description("A great contribution")
+                .sugarDegree(10.0)
+                .quantity(-100.0)
+                .associatedGrapeType(new GrapeType())
+                .build();
 
-        Contribution contribution = new Contribution(id, origin, country, photoURL, description, sugarDegree, quantity, grapeType);
+        Set<ConstraintViolation<Contribution>> violations = validator.validate(contribution);
+        Assertions.assertEquals(1, violations.size());
+        Assertions.assertEquals("Il contributo deve avere una quantità positiva", violations.iterator().next().getMessage());
+    }
 
-        assertEquals(id, contribution.getId());
-        assertEquals(origin, contribution.getOrigin());
-        assertEquals(country, contribution.getCountry());
-        assertEquals(photoURL, contribution.getPhotoURL());
-        assertEquals(description, contribution.getDescription());
-        assertEquals(sugarDegree, contribution.getSugarDegree());
-        assertEquals(quantity, contribution.getQuantity());
-        assertEquals(grapeType, contribution.getAssociatedGrapeType());
+    @Test
+    public void testContributionWithNullGrapeTypeIsInvalid() {
+        Contribution contribution = Contribution.builder()
+                .origin("Italy")
+                .country("Italy")
+                .photoURL("http://example.com/image.jpg")
+                .description("A great contribution")
+                .sugarDegree(10.0)
+                .quantity(100.0)
+                .associatedGrapeType(null)
+                .build();
+
+        Set<ConstraintViolation<Contribution>> violations = validator.validate(contribution);
+        Assertions.assertEquals(1, violations.size());
+        Assertions.assertEquals("Non può essere nullo", violations.iterator().next().getMessage());
+    }
+
+    @Test
+    public void testContributionWithBlankOriginIsInvalid() {
+        Contribution contribution = Contribution.builder()
+                .origin("")
+                .country("Italy")
+                .photoURL("http://example.com/image.jpg")
+                .description("A great contribution")
+                .sugarDegree(10.0)
+                .quantity(100.0)
+                .associatedGrapeType(new GrapeType())
+                .build();
+
+        Set<ConstraintViolation<Contribution>> violations = validator.validate(contribution);
+        Assertions.assertEquals(1, violations.size());
+        Assertions.assertEquals("Il contributo deve avere un' origine", violations.iterator().next().getMessage());
+    }
+
+    @Test
+    public void testContributionWithBlankCountryIsInvalid() {
+        Contribution contribution = Contribution.builder()
+                .origin("Italy")
+                .country("")
+                .photoURL("http://example.com/image.jpg")
+                .description("A great contribution")
+                .sugarDegree(10.0)
+                .quantity(100.0)
+                .associatedGrapeType(new GrapeType())
+                .build();
+
+        Set<ConstraintViolation<Contribution>> violations = validator.validate(contribution);
+        Assertions.assertEquals(1, violations.size());
+        Assertions.assertEquals("Il campo nazione non può essere vuoto", violations.iterator().next().getMessage());
+    }
+
+    @Test
+    public void testContributionWithNegativeSugarDegreeIsInvalid() {
+        Contribution contribution = Contribution.builder()
+                .origin("Italy")
+                .country("Italy")
+                .photoURL("http://example.com/image.jpg")
+                .description("A great contribution")
+                .sugarDegree(-10.0)
+                .quantity(100.0)
+                .associatedGrapeType(new GrapeType())
+                .build();
+
+        Set<ConstraintViolation<Contribution>> violations = validator.validate(contribution);
+        Assertions.assertEquals(1, violations.size());
+        Assertions.assertEquals("Il contributo deve avere una quantità positiva", violations.iterator().next().getMessage());
     }
 }
