26ee1f36bf49c6dc30ce461ee8d6d56236013ce8
Angelo-Conca <angelo.conca@gmail.com>
2023-05-17 15:01:04 +0200

    prima del marge con develop

--- vino/src/test/java/it/unimol/vino/controller/ContributionControllerTest.java
+++ vino/src/test/java/it/unimol/vino/controller/ContributionControllerTest.java
@@ -2,14 +2,15 @@ package it.unimol.vino.controller;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import it.unimol.vino.models.entity.Contribution;
 import it.unimol.vino.models.entity.GrapeType;
 import it.unimol.vino.models.entity.Provider;
+import it.unimol.vino.repository.ContributionRepository;
 import it.unimol.vino.repository.GrapeTypeRepository;
 import it.unimol.vino.repository.ProviderRepository;
 import it.unimol.vino.repository.UserRepository;
 import it.unimol.vino.utils.AuthToken;
 import it.unimol.vino.models.request.RegisterContributionRequest;
-import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -17,20 +18,19 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Date;
-import java.util.Optional;
+
 
 import org.junit.Test;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 
 
-import static org.mockito.Mockito.when;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ActiveProfiles("h2")
@@ -38,6 +38,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 public class ContributionControllerTest {
+    @Autowired
+    private ContributionRepository contributionRepository;
     @Autowired
     private UserRepository userRepository;
     @Autowired
@@ -48,7 +50,7 @@ public class ContributionControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @Autowired
     private GrapeTypeRepository grapeTypeRepository;
 
 
@@ -62,14 +64,13 @@ public class ContributionControllerTest {
     }
 
     @Test
-    @Transactional
     public void putContributionTest() throws Exception {
         Provider provider = new Provider();
         provider.setEmail("a.a@c");
         providerRepository.save(provider);
         RegisterContributionRequest contribution = new RegisterContributionRequest();
         GrapeType grapeType = GrapeType.builder().type("Salvatore").species("Nera").color("nera").build();
-        when(grapeTypeRepository.findById("Salvatore")).thenReturn(Optional.of(grapeType));
+        grapeTypeRepository.save(grapeType);
         contribution.setOrigin("Italia");
         contribution.setCountry("Campania");
         contribution.setDescription("Frizzante");
@@ -87,5 +88,55 @@ public class ContributionControllerTest {
                         .content(objectMapper.writeValueAsString(contribution)))
                 .andExpect(status().isOk());
     }
-}
+    @Test
+    public void putContributionTestBadRequest() throws Exception {
+        Provider provider = new Provider();
+        provider.setEmail("a.b@c");
+        providerRepository.save(provider);
+        GrapeType grapeType = GrapeType.builder().type("Salvatore").species("Bianca").color("nera").build();
+        grapeTypeRepository.save(grapeType);
+        RegisterContributionRequest contribution = new RegisterContributionRequest();
+        contribution.setCountry("Campania");
+        contribution.setDescription("Frizzante");
+        contribution.setSugarDegree(5.5);
+        contribution.setQuantity(10.0);
+        contribution.setDate(new Date());
+        contribution.setGrapeTypeId("Salvatore");
+        contribution.setProviderId(provider.getId());
+        objectMapper = new ObjectMapper();
+        tokenClass = new AuthToken(userRepository);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("http://localhost:8080/api/v1/contribution")
+                        .header("Authorization", "Bearer " +tokenClass.generateToken() )
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(contribution)))
+                .andExpect(status().isBadRequest());
+    }
+   @Test
+    public void getAllContributionTest() throws Exception {
+       Provider provider = new Provider();
+       provider.setEmail("a.c@c");
+       providerRepository.save(provider);
+       GrapeType grapeType = GrapeType.builder().type("Salvatore").species("Bianca").color("nera").build();
+       grapeTypeRepository.save(grapeType);
+        tokenClass = new AuthToken(userRepository);
+        Contribution contribution=new Contribution();
+        contribution.setOrigin("Italia");
+        contribution.setCountry("Campania");
+        contribution.setDescription("Frizzante");
+        contribution.setSugarDegree(5.5);
+        contribution.setQuantity(10.0);
+        contribution.setDate(new Date());
+       contribution.setProvider(provider);
+       contribution.setAssociatedGrapeType();
+        contributionRepository.save(contribution);
+            mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/contribution")
+                    .header("Authorization", "Bearer " + tokenClass.generateToken()))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType("application/json"))
+                    .andExpect(jsonPath("$[0].description").value("Frizzante"));
+            // Verifica ulteriori attributi o dati se necessario
+        }
+
+    }
 
