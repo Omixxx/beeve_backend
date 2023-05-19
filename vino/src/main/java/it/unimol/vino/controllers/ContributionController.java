2fcc880180bac707041eaa6556af1d53905b9ac7
Mattia <mattia.natale225@gmail.com>
2023-05-19 23:12:47 +0200

    minor fixes

--- vino/src/main/java/it/unimol/vino/controllers/ContributionController.java
+++ vino/src/main/java/it/unimol/vino/controllers/ContributionController.java
@@ -30,7 +30,7 @@ public class ContributionController {
 
     @PostMapping
     public ResponseEntity<String> put(@ModelAttribute RegisterContributionRequest request) {
-        return ResponseEntity.ok(this.contributionService.put(request).orElseThrow());
+        return ResponseEntity.ok(this.contributionService.put(request));
     }
 
     @GetMapping("/{id}")
